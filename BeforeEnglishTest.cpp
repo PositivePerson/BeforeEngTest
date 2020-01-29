539c089ccc1e55591f74a229abe7b80e01f5c4c9
PositivePerson <57919939+PositivePerson@users.noreply.github.com>
2020-01-30 00:39:53 +0100

    Coded text files required to updated exe

--- BeforeEnglishTest.cpp
+++ BeforeEnglishTest.cpp
@@ -4,6 +4,7 @@
 #include <windows.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
 void clearAr(string *ar){
@@ -21,7 +22,7 @@ void write(string &writing, int time=0)
         Sleep(time);
     }
 }
-
+/*
 void hintOut(string *currentEngWord,int index, string writing=""){
 	writing=currentEngWord[index][0];
 	writing+=" ";
@@ -30,6 +31,130 @@ void hintOut(string *currentEngWord,int index, string writing=""){
 		else writing+="_ ";
 	} 
 	writing+=currentEngWord[index][currentEngWord[index].length()-1];
+	write(writing);
+}*/
+
+void firstHint(string *currentEngWord,int index, int *globalUnveils, int &globalLetters){
+	bool repeat=false;
+	int arIndex=0;
+	string writing="";
+	int wordLength=currentEngWord[index].length();
+	int dashes;
+	globalLetters=0;
+	
+	if(currentEngWord[index].length()==2){
+		writing+="_ " + currentEngWord[index][1];
+	}
+	else{
+		//-------------unveil-all-em-dashes----------------------
+		for(int i=0;i<currentEngWord[index].length();i++){
+			if(currentEngWord[index][i]=='-'){
+				globalLetters++;
+				globalUnveils[globalLetters-1] = i;
+				//wordLength--;
+			}
+		}
+		
+		dashes=globalLetters;
+		
+		//for(int i=0;i<globalLetters;i++) cout<<globalUnveils[i]<<" ";
+		//cout<<'\n';
+		
+		if(wordLength%2!=0){
+			//----unveil (length/2) chars
+			globalLetters = wordLength/2;
+			//cout<<"globalLetters: "<<letters<<'\n';
+		}
+		else{
+			//----unveil (length/2-1) chars
+			globalLetters = wordLength/2-1;
+			//cout<<"letters: "<<letters<<'\n';
+		}
+			
+			//for(int i=0;i<letters;i++) globalUnveils[i]=-1;
+			
+			for(int i=dashes;i<globalLetters;i++){
+				do{
+				repeat=false;
+				globalUnveils[i]=(rand()% (wordLength));
+				for(int j=0;j<i;j++){
+					if(globalUnveils[j]==globalUnveils[i]){
+					 	repeat=true;
+						break;
+					}
+				}
+				}while(repeat);
+			}
+			
+			//for(int i=0;i<globalLetters;i++) cout<<globalUnveils[i]<<" ";
+		//cout<<'\n';
+			
+			sort(globalUnveils, globalUnveils+globalLetters);
+			
+			if(globalUnveils[arIndex]==0){
+				writing=currentEngWord[index][0];
+				arIndex++;
+			}
+			else writing="_";
+			
+			for(int i=1;i<wordLength;i++){
+				if(globalUnveils[arIndex]==i){
+					writing+=" ";
+					writing+=currentEngWord[index][i];
+					arIndex++;
+				}
+				else{
+				writing+=" _";
+				}
+			}
+	}
+		cout<<"	";
+		write(writing);
+}
+
+void secondHint(string *currentEngWord,int index, int *globalUnveils, int &globalLetters){
+	bool repeat=false;
+	int arIndex=0;
+	string writing="";
+		
+	if(currentEngWord[index].length()<3){
+		writing+="Second hint does not exist";
+	}
+	else{
+			globalLetters++;
+			//for(int i=0;i<globalLetters;i++) globalUnveils[i]=-1;
+			
+				do{
+				repeat=false;
+				globalUnveils[globalLetters-1]=(rand()% (currentEngWord[index].length()));
+				for(int j=0;j<globalLetters-1;j++){
+					if(globalUnveils[j]==globalUnveils[globalLetters-1]){
+					 	repeat=true;
+						break;
+					}
+				}
+				}while(repeat);
+			
+			sort(globalUnveils, globalUnveils+globalLetters);
+			
+			if(globalUnveils[arIndex]==0){
+				writing=currentEngWord[index][0];
+				arIndex++;
+			}
+			else writing="_";
+			
+			for(int i=1;i<currentEngWord[index].length();i++){
+				if(globalUnveils[arIndex]==i){
+					writing+=" ";
+					writing+=currentEngWord[index][i];
+					arIndex++;
+				}
+				else{
+				writing+=" _";
+				}
+			}
+	}
+	cout<<"	";
 	write(writing);
 }
 
@@ -55,12 +180,35 @@ void splitUpWords(string &engWord, string *currentEngWord, int &answers){
 		}while(pos<50);
 }
 
+string uncodeIt(string codedWord, int move=5){
+	string result="";
+	int newChar;
+	
+	for(int i=0;i<codedWord.length();i++){
+		if(codedWord[i]==(char)(37)) result+=(char)(32);
+		else if(codedWord[i]==(char)(163)) result+=(char)(79);
+		else if(codedWord[i]==(char)(161)) result+=(char)(82);
+		else if(codedWord[i]==(char)(50)) result+=(char)(45);
+		else{
+			newChar=(int)codedWord[i]-move;
+			if(newChar<97){
+				result+=(char)(newChar+26);
+			}
+			else result+=(char)(newChar);
+		}
+	}
+	return result;
+}
+
 bool doesWordAppeared(string *currentEngWord, int index, string word){
 	for(int i=0;i<index;i++){
 		if(currentEngWord[i]==word) return true;
 	}
 	return false;
 }
+
+int globalLetters=0;
+int *globalUnveils=new int[globalLetters];
 
 string currentEngWord[5];
 
@@ -124,25 +272,26 @@ int main(){
 	cout<<"\n	";
 	
 	do{
+		whichSubject=0;
 	cin>>whichSubject;
 	//whichSubject=_getche()-48;
 	
 	switch(whichSubject){
 		case 1:
-			choosenEngFile="engFamily.txt";
-			choosenUrFile="polFamily.txt";
+			choosenEngFile="engFamilyCoded.txt";
+			choosenUrFile="polFamilyCoded.txt";
 			break;
 		case 2:
-			choosenEngFile="engHome.txt";
-			choosenUrFile="polHome.txt";
+			choosenEngFile="engHomeCoded.txt";
+			choosenUrFile="polHomeCoded.txt";
 			break;
 		case 3:
-			choosenEngFile="engInHouse.txt";
-			choosenUrFile="polInHouse.txt";
+			choosenEngFile="engInHouseCoded.txt";
+			choosenUrFile="polInHouseCoded.txt";
 			break;
 		case 4:
-			choosenEngFile="engKitchenWare.txt";
-			choosenUrFile="polKitchenWare.txt";
+			choosenEngFile="engKitchenWareCoded.txt";
+			choosenUrFile="polKitchenWareCoded.txt";
 			break;
 		default:
 			cout<<"Mistake in command\n	";
@@ -158,9 +307,13 @@ int main(){
 	ifstream UrLanguage(choosenUrFile.c_str());
 	
 	while(getline(UrLanguage,word) && getline(EnglishWords,engWord)){
+		word = uncodeIt(word);
+		engWord = uncodeIt(engWord);
+		
 		clearAr(currentEngWord);
 		countIncorrect=1;
 		answers=0;
+		wantHint=0;
 		
 		//\
 		cout<<"PL: "<<word<<'\n';\
@@ -203,11 +356,12 @@ int main(){
 		cout<<"\n\n";
 		*/
 		
-		writing="'" + word + "' to po angielsku: ";
+		writing=" '" + word + "' to po angielsku:\n";
 		write(writing);
 		
 		//------------input_answer-----------------------------
 		do{
+		cout<<"	";
 		getline(cin, inputAnswer);
 		
 		if(inputAnswer=="end" && !doesWordAppeared(currentEngWord, answers, "end")){
@@ -216,6 +370,7 @@ int main(){
 		}
 		
 		if(inputAnswer=="exit" && !doesWordAppeared(currentEngWord, answers, "exit")){
+			delete[] globalUnveils;
 			_Exit(0);
 		}
 	
@@ -232,26 +387,27 @@ int main(){
 		
 		
 		if(!correct){
-			cout<<"not correct\n";
+			cout<<"	not correct\n";
 			
-			if(countIncorrect==3 || (countIncorrect>3 && wantHint==0)){
-		//-------------hint_suggestion----------------------
-				writing="Do you want to get a hint?\n";
+			if(countIncorrect==2 || (countIncorrect>2 && wantHint==0)){
+		//--------------first_hint_suggestion-----------------
+				writing="	Do you want to get a hint?\n";
 				write(writing);
 				
-				cout<<"1 - yes			0 - no\n";
+				cout<<"	1 - yes			0 - no\n";
 				
 				do{
+				cout<<"	";
 				wantHint=_getche()-48;
 				cout<<'\n';
 				
 				if(wantHint==1){
-					hintOut(currentEngWord, 0);
+					firstHint(currentEngWord, 0, globalUnveils, globalLetters);
 					
 					if(answers>1)
 					for(int i=1;i<answers;i++){
 						cout<<"	or	";
-						hintOut(currentEngWord, i);
+						firstHint(currentEngWord, i, globalUnveils, globalLetters);
 					}
 					
 					cout<<'\n';
@@ -262,22 +418,59 @@ int main(){
 				}
 				
 				else{
-					writing="Choose 1 or 0 (again)\n";
+					writing="	Choose 1 or 0 (again)\n";
 					write(writing);
 				}
 				
 				}while(wantHint!=0 && wantHint!=1);
 			}
 			
-			else if(countIncorrect>3 && wantHint==1){
-				cout<<"skip?\n";
-				cout<<"1 - yes			0 - no\n";
+			else if(countIncorrect==3 || (countIncorrect>3 && wantHint==1)){
+		//--------------second_hint_suggestion-----------------
+				writing="	Do you want to get one more hint?\n";
+				write(writing);
+				
+				cout<<"	1 - yes			0 - no\n";
 				
 				do{
-				wantHint=_getche()-48;
+				cout<<"	";
+				wantHint=_getche()-47;
 				cout<<'\n';
 				
-				if(wantHint==1){
+				if(wantHint==2){
+					secondHint(currentEngWord, 0, globalUnveils, globalLetters);
+					
+					if(answers>1)
+					for(int i=1;i<answers;i++){
+						cout<<"	or	";
+						secondHint(currentEngWord, i, globalUnveils, globalLetters);
+					}
+					
+					cout<<'\n';
+				}
+				
+				else if(wantHint==1){
+					
+				}
+				
+				else{
+					writing="	Choose 1 or 0 (again)\n";
+					write(writing);
+				}
+				
+				}while(wantHint!=1 && wantHint!=2);
+			}
+			
+			else if(countIncorrect>3 && wantHint==2){
+				cout<<"	skip?\n";
+				cout<<"	1 - yes			0 - no\n";
+				
+				do{
+				cout<<"	";
+				wantHint=_getche()-46;
+				cout<<'\n';
+				
+				if(wantHint==3){
 					blanks++;
 					wordsToLearn[toLearnIndex]++;
 					for(int i=0;i<answers;i++) cout<<"	"<<currentEngWord[i]<<'\n';
@@ -286,21 +479,21 @@ int main(){
 					goto surrender;
 				}
 				
-				else if(wantHint==0){
-					wantHint=1;
+				else if(wantHint==2){
+					//wantHint=1;
 				}
 				
 				else{
-					writing="Yes(1) or No(0)\n";
+					writing="	Yes(1) or No(0)\n";
 					write(writing);
 				}
 				
-				}while(wantHint!=0 && wantHint!=1);
+				}while(wantHint!=2 && wantHint!=3);
 			}
 			countIncorrect++;
 		}
 		else{
-			
+		//-----------------count-final-scores------------
 			if(countIncorrect==1){
 				spot_on++;
 			}
@@ -310,7 +503,7 @@ int main(){
 				wordsToLearn[toLearnIndex]++;
 			}
 			
-			cout<<"correct\n\n";
+			cout<<"	correct\n\n";
 			Sleep(175);
 			system("CLS");
 		} 
@@ -361,6 +554,7 @@ int main(){
 		if(endChoice==3){
 			cout<<"\x1B[2J\x1B[H";
 			writing="See ya";
+			delete[] globalUnveils;
 			write(writing);
 			_Exit(0);
 		}
@@ -381,16 +575,18 @@ int main(){
 		if(wordsToLearn[i]==1){
 			//cout<<"engWordsToLearn: "<<engWordsToLearn[i]<<'\n';
 			
-			writing="'" + urWordsToLearn[i] + "' to po angielsku: ";
+			writing=" '" + urWordsToLearn[i] + "' to po angielsku:\n";
 			write(writing);
 			
 			clearAr(currentEngWord);
 			countIncorrect=1;
 			answers=0;
+			wantHint=0;
 			
 			splitUpWords(engWordsToLearn[i], currentEngWord, answers);
 			
 			do{
+		cout<<"	";
 		getline(cin, inputAnswer);
 		
 		if(inputAnswer=="end" && !doesWordAppeared(currentEngWord, answers, "end")){
@@ -399,6 +595,7 @@ int main(){
 		}
 		
 		if(inputAnswer=="exit" && !doesWordAppeared(currentEngWord, answers, "exit")){
+			delete[] globalUnveils;
 			_Exit(0);
 		}
 	
@@ -415,26 +612,27 @@ int main(){
 		
 		
 		if(!correct){
-			cout<<"not correct\n";
+			cout<<"	not correct\n";
 			
-			if(countIncorrect==3 || (countIncorrect>3 && wantHint==0)){
-		//-------------hint_suggestion----------------------
-				writing="Do you want to get a hint?\n";
+			if(countIncorrect==2 || (countIncorrect>2 && wantHint==0)){
+		//--------------first_hint_suggestion-----------------
+				writing="	Do you want to get a hint?\n";
 				write(writing);
 				
-				cout<<"1 - yes			0 - no\n";
+				cout<<"	1 - yes			0 - no\n";
 				
 				do{
+				cout<<"	";
 				wantHint=_getche()-48;
-				cout<<'\r'<<'\n';
+				cout<<'\n';
 				
 				if(wantHint==1){
-					hintOut(currentEngWord, 0);
+					firstHint(currentEngWord, 0, globalUnveils, globalLetters);
 					
 					if(answers>1)
 					for(int i=1;i<answers;i++){
 						cout<<"	or	";
-						hintOut(currentEngWord, i);
+						firstHint(currentEngWord, i, globalUnveils, globalLetters);
 					}
 					
 					cout<<'\n';
@@ -445,38 +643,75 @@ int main(){
 				}
 				
 				else{
-					writing="Choose 1 or 0 (again)\n";
+					writing="	Choose 1 or 0 (again)\n";
 					write(writing);
 				}
 				
 				}while(wantHint!=0 && wantHint!=1);
 			}
 			
-			else if(countIncorrect>3 && wantHint==1){
-				cout<<"skip?\n";
-				cout<<"1 - yes			0 - no\n";
+			else if(countIncorrect==3 || (countIncorrect>3 && wantHint==1)){
+		//--------------second_hint_suggestion-----------------
+				writing="	Do you want to get one more hint?\n";
+				write(writing);
+				
+				cout<<"	1 - yes			0 - no\n";
 				
 				do{
-				wantHint=_getche()-48;
+				cout<<"	";
+				wantHint=_getche()-47;
 				cout<<'\n';
 				
-				if(wantHint==1){
+				if(wantHint==2){
+					secondHint(currentEngWord, 0, globalUnveils, globalLetters);
+					
+					if(answers>1)
+					for(int i=1;i<answers;i++){
+						cout<<"	or	";
+						secondHint(currentEngWord, i, globalUnveils, globalLetters);
+					}
+					
+					cout<<'\n';
+				}
+				
+				else if(wantHint==1){
+					
+				}
+				
+				else{
+					writing="	Choose 1 or 0 (again)\n";
+					write(writing);
+				}
+				
+				}while(wantHint!=1 && wantHint!=2);
+			}
+			
+			else if(countIncorrect>3 && wantHint==2){
+				cout<<"	skip?\n";
+				cout<<"	1 - yes			0 - no\n";
+				
+				do{
+				cout<<"	";
+				wantHint=_getche()-46;
+				cout<<'\n';
+				
+				if(wantHint==3){
 					blanks++;
 					//wordsToLearn[toLearnIndex]++;
 					system("CLS");
 					goto surrender1;
 				}
 				
-				else if(wantHint==0){
+				else if(wantHint==2){
 					wantHint=1;
 				}
 				
 				else{
-					writing="Yes(1) or No(0)\n";
+					writing="	Yes(1) or No(0)\n";
 					write(writing);
 				}
 				
-				}while(wantHint!=0 && wantHint!=1);
+				}while(wantHint!=2 && wantHint!=3);
 			}
 			countIncorrect++;
 		}
@@ -491,7 +726,7 @@ int main(){
 				//wordsToLearn[toLearnIndex]++;
 			}
 			
-			cout<<"correct\n\n";
+			cout<<"	correct\n\n";
 			Sleep(175);
 			system("CLS");
 		}
